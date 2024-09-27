fae454798a0f321c0228371d0a77b1237d58ce3a
HARSH <harshsennn@gmail.com>
2024-09-27 09:23:59 +0530

    push

--- Patterns.java
+++ Patterns.java
@@ -36,3 +36,41 @@ static void pattern2(int n){
 }
 }
   
+******
+*****
+****
+***
+**
+*
+
+public class Patterns {
+    public static void main(String args[]){
+    pattern3(5);
+    }
+static void pattern3(int n){
+    for(int row = 0; row <= n; row++){
+        for(int cols = n;  cols >= row; cols--){
+         System.out.print("*");
+        } System.out.println();
+}
+}
+}
+
+1
+12
+123
+1234
+12345
+
+public class Patterns {
+    public static void main(String args[]){
+    pattern4(5);
+    }
+static void pattern4(int n){
+    for(int row = 0; row <= n; row++){
+        for(int cols = 1;  cols <= row; cols++){
+         System.out.print(cols);
+        } System.out.println();
+}
+}
+}
