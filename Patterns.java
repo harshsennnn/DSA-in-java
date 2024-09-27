4a827b93d35673a3adfe00975f54f7818a95bc2c
HARSH <harshsennn@gmail.com>
2024-09-27 09:12:22 +0530

    patterns commit 2

--- Patterns.java
+++ Patterns.java
@@ -16,5 +16,23 @@ static void pattern1(int n){
 }
 }
 
+*
+**
+***
+****
+*****
+******
 
+public class Patterns {
+    public static void main(String args[]){
+    pattern2(5);
+    }
+static void pattern2(int n){
+    for(int row = 0; row <= n; row++){
+        for(int cols = 0;  cols <= row; cols++){
+         System.out.print("*");
+        } System.out.println();
+}
+}
+}
   
