e2243e3302a6217c3d7f1ebff5e728cd39eced67
HARSH <harshsennn@gmail.com>
2024-09-27 08:54:06 +0530

    patterns commit

---
+++ Patterns.java
@@ -0,0 +1,20 @@
+*****
+*****
+*****
+*****
+*****
+public class Patterns {
+    public static void main(String args[]){
+    pattern1(5);
+    }
+static void pattern1(int n){
+    for(int row = 0; row <= n; row++){
+        for(int cols = 0;  cols <= n; cols++){
+         System.out.print("*");
+        } System.out.println();
+}
+}
+}
+
+
+  
