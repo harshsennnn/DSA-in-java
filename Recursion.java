e5a89f083cba096397db01937735d6972d1fdcf9
HARSH <harshsennn@gmail.com>
2024-10-06 17:40:39 +0530

    Recursion update

--- Recursion.java
+++ Recursion.java
@@ -4,3 +4,16 @@
    - Use debugger to understand the flow of program */
 
    /* NOTE: Try to break the problem into smaller chunks  */
+
+   // 1342. Number of Steps to Reduce a Number to Zero : https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/
+
+   class Solution {
+    public int numberOfSteps(int num) {
+      return helper(num, 0);  
+    }
+private int helper(int num, int steps){
+    if(num == 0){return steps;}
+    if(num % 2 == 0){return helper(num/2, ++steps);}
+    return helper(num - 1, ++steps);
+}
+}
